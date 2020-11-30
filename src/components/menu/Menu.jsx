dbd733ff6366022c413c429550da51c14b08e033
Alexey <apill@ukr.net>
2020-11-30 16:46:25 +0200

    3hours left

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,25 +5,8 @@ const Menu = () => {
 
   return (
       <div className='menu'>
-        <div className='menu__header'></div>
-        <div className='menu__container'> 
-          <div className='menu__title'><a href="#reg">About me</a></div>
-          <div className='menu__title'>Relationships</div>
-          <div className='menu__title'>Users</div>
-          <div className='menu__title'>Sign Up</div>
-          <div className='menu__title'>Terms and Conditions</div>
-          <div className='menu__line'></div>
-          <div className='menu__title'>How it works</div>
-          <div className='menu__title'>Partnership</div>
-          <div className='menu__title'>Help</div>
-          <div className='menu__title'>Leave testimonial</div>
-          <div className='menu__title'>Contact us</div>
-          <div className='menu__line'></div>
-          <div className='menu__title'>Articles</div>
-          <div className='menu__title'>Our news</div>
-          <div className='menu__title'>Testimonials</div>
-          <div className='menu__title'>Licenses</div>
-          <div className='menu__title'>Privacy Policy</div>
+        <div className='menu__container'> TEST
+          <div className='menu__title'>Test</div>
         </div>
       </div>
   )
--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,17 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import "./page.scss";
+import Menu from '../menu/Menu';
 
 const Page = () => {
+
+ const [showMenu, setShowMenu] = useState(false)
+
+  const handleClick = () => {
+    setShowMenu(showMenu ? false : true);
+  }
+
 
   return (
       <div className='page'>
         <div className='page__container'>
-        <div className="page__container_header">logo</div>
-        <div className="page__sidebar">
-           <div className="page__sidebar_logo">logo</div>
-           
-        </div>
-          
+            <div className="page__container_header">
+               {showMenu ? <Menu /> : null}
+                <div className="page__container_header-loopicon">
+                    <i className='fa fa-search' ></i>
+                </div>
+                <div className="page__container_header-icon1">
+                <i className="fa fa-calendar" aria-hidden="true"></i>
+                </div>
+                <div className="page__container_header-icon2">
+                <i className="fa fa-bell" aria-hidden="true"></i>
+                </div>
+                <div className="page__container_header-usericon">
+                    <i className="fa fa-user" ></i>
+                </div>
+            </div>
+            <div className="page__sidebar">
+                <div className="page__sidebar_logo">
+                    <img className="page__sidebar_logo-img" src="logo1.png" alt="logo1" onClick={handleClick}></img>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+                <div className="page__sidebar_itemcontainer">
+                    <div className="page__sidebar_itemcontainer-item"></div>
+                </div>
+            </div>
+            <div className="page__container_block1"></div>
+            <div className="page__container_gridcontainer">
+                <div className="page__container_gridcontainer-block1"></div>
+                <div className="page__container_gridcontainer-block2"></div>
+                
+            </div>
+
         </div>
       </div>
   )
